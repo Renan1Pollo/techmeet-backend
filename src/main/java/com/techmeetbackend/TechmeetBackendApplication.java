960821de3492a7fa1bf77039680f27f8f77ae93c
Renan Pollo Benelli <renansk18@outlook.com>
2024-05-31 19:47:38 -0300

    first commit

---
+++ src/main/java/com/techmeetbackend/TechmeetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techmeet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmeetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techmeetbackend/TechmeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmeet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
